89dfde07f0a5e24d19dcf3d9ae3dc653c8ff3984
Artem Vetik <artem.vetik58@gmail.com>
2023-10-06 20:30:55 +0300

    Make RemoveServerController private

---
+++ WebApi/WebApi/Controllers/RestrictToLocalhostAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApi.Controllers
+{
+    public class RestrictToLocalhostAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var remoteIp = context.HttpContext.Connection.RemoteIpAddress.MapToIPv4();
+
+            if (IsLocal(remoteIp.GetAddressBytes()) == false)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            base.OnActionExecuting(context);
+        }
+
+        private static bool IsLocal(byte[] ipv4)
+        {
+            return ipv4[0] == 172 && (ipv4[1] & 0xf0) == 16;
+        }
+    }
+}
